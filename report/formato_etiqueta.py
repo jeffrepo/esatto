e3742170ca9de8e5042eef3af8415ba046165179
Geordie Castaneda <jordyorantes9@gmail.com>
2023-04-15 22:36:06 -0600

    Ajustes Etiquetas

--- report/formato_etiqueta.py
+++ report/formato_etiqueta.py
@@ -22,30 +22,32 @@ class ReportEsattoEtiqueta(models.AbstractModel):
         logging.warning(docs.picking_ids)
 
         for sale in docs:
-            logging.warning(sale.name)
-            if sale.id not in dicc_etiquetas:
-                dicc_etiquetas[sale.id]={
-                'name': sale.name,
-                'kit': {},
-                'nombre_paquetes':''
-                }
-            nombres_paquete = []
-            for line in docs.picking_ids.move_ids_without_package:
-                logging.warning(line.bom_line_id)
-                logging.warning(line.bom_line_id.bom_id.display_name)
-                logging.warning('')
-                if sale.id in dicc_etiquetas:
-                    if line.bom_line_id.bom_id.id not in dicc_etiquetas[sale.id]['kit']:
-                        dicc_etiquetas[sale.id]['kit'][line.bom_line_id.bom_id.id]={
-                        'name_kit':line.bom_line_id.bom_id.display_name
-                        }
-                        nombres_paquete.append(line.bom_line_id.bom_id.display_name)
-                        dicc_etiquetas[sale.id]['nombre_paquetes'] = " ".join(nombres_paquete)
+            for linea_materiales in sale.picking_ids.move_ids_without_package:
+                if linea_materiales.x_studio_paquete not in dicc_etiquetas:
+                    dicc_etiquetas[linea_materiales.x_studio_paquete]={
+                    'numero_paquete': linea_materiales.x_studio_paquete,
+                    'numero_paquetes': sale.picking_ids.x_studio_numero_de_paquetes,
+                    'productos':[]
+                    }
+                dicc_etiquetas[linea_materiales.x_studio_paquete]['productos'].append(linea_materiales)
 
 
-
-        logging.warning('')    
-        logging.warning(dicc_etiquetas)
+            # logging.warning(sale.name)
+            # if sale.id not in dicc_etiquetas:
+            #     dicc_etiquetas[sale.id]={
+            #     'name':sale.name,
+            #     'paquete_id':{},
+            #     'names': ''
+            #     }
+            # lst_names = []
+            # for linea in sale.order_line:
+            #     if linea.product_template_id.bom_ids:
+            #         if linea. not in dicc_etiquetas[sale.id]['paquete_id']:
+            #             dicc_etiquetas[sale.id]['paquete_id'][linea.x_studio_paquete]=[]
+            #
+            #         for linea_materiales in sale.picking_ids.move_ids_without_package:
+            #
+            #             dicc_etiquetas[sale.id]['paquete_id'][linea.x_studio_paquete].append(linea_materiales)
 
         return {
             'doc_ids': docids,
